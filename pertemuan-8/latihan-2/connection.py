c78b71735c0f1554fb96e7cd30b5d18efdd848aa
Shendy Fatihara <fatiharashendy@gmail.com>
2025-04-30 21:22:33 +0700

    Create connection.py

---
+++ pertemuan-8/latihan-2/connection.py
@@ -0,0 +1,26 @@
+import mysql.connector
+
+class DatabaseConnection:
+    def __init__(self, host, user, password, database):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.connection = None
+
+    def connect(self):
+        try:
+            self.connection = mysql.connector.connect(
+                host=self.host,
+                user=self.user,
+                password=self.password,
+                database=self.database
+            )
+            print("Database connected successfully.")
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+
+    def disconnect(self):
+        if self.connection:
+            self.connection.close()
+            print("Database disconnected.")
